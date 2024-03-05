655ef376b68c86ad17bde4c3b3c70f5c7f8ddc79
libSpace <malikmphomalope@gmail.com>
2024-03-05 16:24:56 +0200

    Impemented functions and methods that communicates with my back end

--- client-x/src/Components/SignUp.jsx
+++ client-x/src/Components/SignUp.jsx
@@ -1,22 +1,61 @@
-function SignUp() {
+import { useState } from 'react';
 
+function SignUp() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+      });
+
+      if (response.ok) {
+        // Handle successful login
+        console.log('SignUp successful');
+        
+      } else {
+        // Handle login error
+        console.error('SignUp failed');
+      }
+    } catch (error) {
+      console.error('Error:', error.message);
+    }
+  };
 
   return (
     <div>
       
-      <form className="m-5">
+      <form onSubmit={handleSubmit} className="m-5">
             <h1>Sign Up</h1>
         <div className="form-group">
-          <input type="email" className="form-control" placeholder="Enter email" id="email" />
+          <input 
+            type="text" 
+            className="form-control" 
+            placeholder="Enter username" 
+            value={username} 
+            onChange={(e) => 
+            setUsername(e.target.value)} />
         </div>
         <div className="form-group">
-          <input type="password" className="form-control" placeholder="Enter password" id="pwd" />
+          <input 
+            type="password" 
+            className="form-control" 
+            placeholder="Enter password" 
+            value={password} 
+            onChange={(e) => setPassword(e.target.value)} />
         </div>
-        <div className="form-group form-check">
+        {/* <div className="form-group form-check">
           <label className="form-check-label">
             <input className="form-check-input" type="checkbox" /> Remember me
           </label>
-        </div>
+        </div> */}
         <button type="submit" className="btn btn-primary">Submit</button>
       </form>
     </div>
